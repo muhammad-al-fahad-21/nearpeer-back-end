51cc3bb095482769ad1c22d8f39b68de15a539e8
Muhammad Al Fahad <muhammad.alfahad@nearpeer.org>
2023-08-15 15:05:28 +0500

    Update model/index.js

--- src/api/models/index.js
+++ src/api/models/index.js
@@ -9,7 +9,17 @@ const dbConfig = config[env];
 
 const db = {};
 
-const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig);
+const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+    host: dbConfig.host,
+    port: dbConfig.port,
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+        require: true,   // This enforces SSL
+        rejectUnauthorized: false   // Allows self-signed certificates
+        }
+    }
+});
 
 fs
     .readdirSync(__dirname)
