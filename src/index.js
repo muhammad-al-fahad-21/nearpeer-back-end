d8d3abfa3784aaaf3dc24b413035b2e94b82e0ff
Muhammad Al Fahad <muhammad.alfahad@nearpeer.org>
2023-08-15 13:35:12 +0500

    changes to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require('./config/express');
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5005
 
 const server = app.listen(PORT, () => {
     console.log(`Server running on ${PORT} `)
